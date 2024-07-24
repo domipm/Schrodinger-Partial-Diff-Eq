0b0824c171a4c17ed2cf766b06d26dee4dfdf436
domipm <dpas602@gmail.com>
2024-07-24 21:26:31 +0200

    Commented code better in english

--- schrodinger.cpp
+++ schrodinger.cpp
@@ -9,10 +9,10 @@
 
 int N, n_ciclos, n_temp;
 double lam, k, s, norm = 0;
-double V[MAX]; //VECTOR POTENCIAL
+double V[MAX]; // Array of potential values
 
-fcomplex fonda[MAX]; //FUNCIÓN DE ONDA (Complejo en general)
-fcomplex A[MAX], alpha[MAX], B[MAX], beta[MAX], chi[MAX]; //COEFICIENTES
+fcomplex fonda[MAX];                                        // Wave function (complex)
+fcomplex A[MAX], alpha[MAX], B[MAX], beta[MAX], chi[MAX];   // Algorithm coefficients
 fcomplex i = Complex(0,1);
 
 using namespace std;
@@ -20,99 +20,85 @@ using namespace std;
 int main() {
 
 
-    // ? F I C H E R O  S A L I D A //
-
+    //  Output files
     FILE *out;
-    out = fopen("fonda.dat", "w"); //FICHERO SALIDA MÓDULO FUNCIÓN DE ONDA
+    out = fopen("fonda.dat", "w");  // Wavefunction modulus
     FILE *pot;
-    pot = fopen("pot.dat", "w"); //FICHERO SALIDA POTENCIAL
+    pot = fopen("pot.dat", "w");    // Potential
     FILE *norma;
-    norma = fopen("norm.dat", "w"); //FICHERO SALIDA NORMAL DE LA FUNCIÓN DE ONDA
+    norma = fopen("norm.dat", "w"); // Wavefunction norm
 
 
-    // ? P A R A M E T R O S  I N I C I A L E S //
+    //  Initial parameters
 
-    N = 1000; //DIVISIONES DEL EJE X
-    n_ciclos = 100; //CICLOS A REALIZAR
-    n_temp = 10000; //NUMERO DE ITERACIONES TEMPORALES
-    lam = 0.8; //PARAMETRO LAMBDA (Altura del potencial)
+    N = 1000;       // X-axis divisions
+    n_ciclos = 100; // Cycles to-do
+    n_temp = 10000; // Number of temporal iterations
+    lam = 0.8;      // Lambda parameter (height of potential)
 
-    k = 2*PI*double(n_ciclos)/double(N); //FACTOR K REESCALADO
-    s = 1/(4*k*k); //ESPACIADO TEMPORAL REESCALADO
+    k = 2*PI*double(n_ciclos)/double(N);    // Rescaling factor K
+    s = 1/(4*k*k);                          // Rescaling of temporal divisions
 
-
-    // ? P O T E N C I A L //
-
-
+    //  Potential
     for (int j = 0; j < N; j++) {
-        if ( (j < 3*N/5 ) && (j > 2*N/5) )  V[j] = lam*k*k; //POTENCIAL DE ALTURA LAMBDA*K*K
-        else V[j] = 0.0; //POTENCIAL NULO FUERA DEL RANGO DEFINIDO
-        fprintf(pot, "%.15lf\n", V[j]); //ESCRIBIMOS EL POTENCIAL EN EL FICHERO
+        if ( (j < 3*N/5 ) && (j > 2*N/5) )  V[j] = lam*k*k; // Potential barrier of height LAMBDA*K*K
+        else V[j] = 0.0;                                    // Null potential outside defined range
+        fprintf(pot, "%.15lf\n", V[j]);                     // Write potential to file
     }
+    //  Close file
+    fclose(pot);
 
-
-    // ? F U N C I Ó N  D E  O N D A  I N I C I A L //
-
-
-    // * C Á L C U L O  D E  L A  F U N C I Ó N //
-
+    //  Initial wave function
     for (int j = 0; j < N; j++) {
-        if (j == 0 || j == (N)) fonda[j] = Complex(0,0); //CONDICIONES DE CONTORNO
+        if (j == 0 || j == (N)) fonda[j] = Complex(0,0);    // Boundary conditions (wavefunction zero at extremes)
         else {
-            fonda[j] = Complex( cos(k*j) * exp( ( -8*double(4*j-N)*double(4*j-N) ) / double(N*N) ) , sin(k*j) * exp( ( -8*double(4*j-N)*double(4*j-N) ) / double(N*N) ) ); //CALCULAMOS FUNCIÓN DE ONDA
-            norm += Cabs(Cmul(fonda[j], Conjg(fonda[j]))); //CALCULAMOS LA NORMA DE LA FUNCIÓN DE ONDA
+            //  Obtain wave function at each position
+            fonda[j] = Complex( cos(k*j) * exp( ( -8*double(4*j-N)*double(4*j-N) ) / double(N*N) ) , sin(k*j) * exp( ( -8*double(4*j-N)*double(4*j-N) ) / double(N*N) ) );
+            norm += Cabs(Cmul(fonda[j], Conjg(fonda[j])));  // Norm of wave function
         }
     }
 
-    // * N O R M A L I Z A C I O N //
-    
+    //  Normalization
     for (int j = 0; j < N; j++) {
-        fonda[j] = Cdiv(fonda[j], Complex(sqrt((norm)),0)); //DIVIDIMOS ENTRE NORMA
-        fprintf(out, "%.15lf\n", Cabs(fonda[j])); //ESCRIBIMOS LA FUNCIÓN DE ONDA YA NORMALIZADA EN CADA PUNTO j DEL EJE x
+        fonda[j] = Cdiv(fonda[j], Complex(sqrt((norm)),0)); // Divide by norm
+        fprintf(out, "%.15lf\n", Cabs(fonda[j]));           // Rewrite normalized wavefunction at each point
     }
-    fprintf(out, "\n"); //LINEA NUEVA PARA SEPARAR LAS ITERACIONES
+    fprintf(out, "\n"); // Newline for new iterations
     
 
-    // * V E C T O R E S  'A_j^0'  Y  'A L P H A' //
-
-    for (int j = 0; j < N; j++) A[j] = Complex( -2-V[j], 2/s ); //VECTOR A_j^0
-    alpha[N-1] = Complex(0.0, 0.0); //CONDICION CONTORNO alpha_N-1 = 0
-    for (int j = N-2; j > 0; j--) alpha[j-1] = Cdiv( Complex(-1.0, 0.0), Cadd(A[j], alpha[j] ) ); //VECTOR alpha_j
-
-
-    // ? A L G O R I T M O //
+    // A_j^0, alpha vectors
+    for (int j = 0; j < N; j++) A[j] = Complex( -2-V[j], 2/s );                                     // Vector A_j^0
+    alpha[N-1] = Complex(0.0, 0.0);                                                                 // Boundary condition alpha_N-1 = 0
+    for (int j = N-2; j > 0; j--) alpha[j-1] = Cdiv( Complex(-1.0, 0.0), Cadd(A[j], alpha[j] ) );   // Vector alpha_j
 
 
+    //  Algorithm to obtain time evolution of wavefunction
     for (int t = 0; t < n_temp; t++) {
 
-        norm = 0; //IGUALAMOS LA NORMA A CERO EN CADA ITERACION
+        norm = 0; // Zero norm at beginning of each iteration
 
-        // * V E C T O R E S  'B_j'  Y  'B E T A' //
+        //  B_j, beta vectors
+        for (int j = 0; j < N; j++) B[j] = Cdiv( Cmul( Complex(0.0, 4.0), fonda[j]), Complex(s, 0.0) ); // Vector B_j
+        beta[N-1] = Complex(0.0, 0.0);                                                                  // Boundary condition beta_N-1 = 0
+        for (int j = N-2; j > 0; j--) beta[j-1] = Cdiv( Csub(B[j], beta[j]), Cadd(A[j], alpha[j]) );    // Vector beta_j
 
-        for (int j = 0; j < N; j++) B[j] = Cdiv( Cmul( Complex(0.0, 4.0), fonda[j]), Complex(s, 0.0) ); //VECTOR B_j
-        beta[N-1] = Complex(0.0, 0.0); //CONDICIÓN DE CONTORNO beta_N-1 = 0
-        for (int j = N-2; j > 0; j--) beta[j-1] = Cdiv( Csub(B[j], beta[j]), Cadd(A[j], alpha[j]) ); //VECTOR beta_j
-
-        // * V E C T O R  'C H I' //
-
+        //  Chi vector
         for (int j = 0; j < N-1; j++) chi[j+1] = Cadd( Cmul(alpha[j], chi[j]), beta[j] );
 
-        // * F U N C I Ó N  D E  O N D A //
-
+        //  Wave function
         for (int j = 0; j < N; j++) {
             fonda[j] = Csub( chi[j], fonda[j] );
             norm += Cabs(Cmul(fonda[j], Conjg(fonda[j])));
         }
 
-        fprintf(norma, "%d\t%.15lf\n", t, sqrt(norm)); //ESCRIBIMOS EN EL FICHERO LA RAIZ DE LA NORMA AL CUADRADO (EN CADA ITERACION)
-        for (int j = 0; j < N; j++) fprintf(out, "%.15lf\n", Cabs(fonda[j])); //ESCRIBIMOS LA FUNCIÓN DE ONDA EN EL FICHERO (SU MÓDULO)
-        fprintf(out, "\n"); //LINEA NUEVA TRAS CADA ITERACIÖN PARA SEPARAR LAS FUNCIONES DE ONDA
+        fprintf(norma, "%d\t%.15lf\n", t, sqrt(norm));                          // Write square root of norm squared for each iteration
+        for (int j = 0; j < N; j++) fprintf(out, "%.15lf\n", Cabs(fonda[j]));   // Write new modulus of wave function
+        fprintf(out, "\n");                                                     // Newlines to separate iterations
 
     }
 
-    //fcloseall(); //CERRAMOS LOS FICHEROS
+    //  Close files
     fclose(out);
-    fclose(pot);
     fclose(norma);
 
     return 0;
--- schrodinger_plot.py
+++ schrodinger_plot.py
@@ -4,23 +4,25 @@ import matplotlib.animation as animation
 
 plt.rcParams["animation.convert_path"] = "/usr/bin/convert"
 
-pot = np.loadtxt("pot.dat") #POTENCIAL (N,V)
-fonda = np.loadtxt("fonda.dat") #FUNCION DE ONDA (N, Re F, Im F) Cada N datos tiempo T+1
+pot = np.loadtxt("pot.dat")     # Potential (N,V)
+fonda = np.loadtxt("fonda.dat") # Wave function (N, Re F, Im F) each N time T+1
 
-N = int(len(pot)) #DIVISIONES DE X
-iter = int(len(fonda)/N) #ITERACIONES REALIZADAS
+N = int(len(pot))           # X-axis divisions
+iter = int(len(fonda)/N)    # Total iterations
 
 skip = 10
 
-x = np.arange(0,N,1) #EJE X
+x = np.arange(0,N,1) # X-axis values range
+
+#   Generate animation
 
 fig, ax = plt.subplots()
 data = []
-ln, = plt.plot([], [], linestyle='-', color="Red", label="$\\phi$")
+ln, = plt.plot([], [], linestyle='-', color="Red", label=r"Wavefunction $\phi$")
 
 def init():
 
-    ax.set_title("Schrödinger")
+    ax.set_title("Schrödinger Equation")
     ax.set_xlabel("$x$")
     ax.set_ylabel("$y$")
     ax.set_xlim(-1, N+1)
@@ -29,8 +31,6 @@ def init():
     return ln,
 
 def update(k):
-
-    #k = numero iteracion
 
     data.clear()
     
@@ -41,9 +41,9 @@ def update(k):
 
     return ln,
 
-plt.plot(x, pot, label="$V$")
+plt.plot(x, pot, label=r"Potential $V$")
 plt.legend(loc="upper right")
 
 anim = animation.FuncAnimation(fig, update, frames=np.arange(0,iter,skip),init_func=init,blit=False)
-anim.save("schrodinger_nopot.gif", writer=animation.PillowWriter(fps=30))
+anim.save("schrodinger.gif", writer=animation.PillowWriter(fps=30), dpi=300)
 #plt.show()
